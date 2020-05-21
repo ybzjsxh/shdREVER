350f170edd94cc31a9edb9e52d1a3e2e8ac4fdad
ybzjsxh <musiq841@gmail.com>
2020-05-21 18:15:34 +0800

    chore: upgrade dependencies, mainly log4js to fix daysToKeep problem

--- back_node/config/db.js
+++ back_node/config/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // const DB_URL = 'mongodb://192.168.204.145:27017/devices'
-const DB_URL = 'mongodb://47.98.142.156:27777/devices'
-mongoose.connect(DB_URL, {useNewUrlParser: true})
-mongoose.connection.on('connected', ()=>{
+const DB_URL = "mongodb://47.98.142.156:27777/devices";
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("connected", () => {
   console.log(`connected to ${DB_URL}`);
-})
+});
 
 module.exports = mongoose;
