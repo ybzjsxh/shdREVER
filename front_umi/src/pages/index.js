0fed8814ec2d40e795878383bf459815d5a3b359
ybzjsxh <musiq841@gmail.com>
2020-07-20 23:41:27 +0800

    chore(deps): upgrade deps

--- front_umi/src/pages/index.js
+++ front_umi/src/pages/index.js
@@ -1,9 +1,5 @@
-import styles from './index.css';
 import Redirect from 'umi/redirect';
 
 export default function() {
-  if (window.__POWERED_BY_QIANKUN__) {
-    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-  }
-  return <Redirect to="/login" className={styles.normal} />;
+  return <Redirect to="/login" />;
 }
